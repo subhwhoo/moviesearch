37009fe3dd9dbec1012f9dfafc45afd5c8951ea4
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-05-13 21:24:09 +0530

    Updates
    Logo Updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+
 
 const Header = () => {
     const [show, setShow] = useState("top");
@@ -74,7 +74,8 @@ const Header = () => {
         <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}>
-                    <img src={logo} alt="" />
+                    {/* <img src={logo} alt="" /> */}
+                    <h2>MovieDB</h2>
                 </div>
                 <ul className="menuItems">
                     <li
